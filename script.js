2d94e221ee369c1a254b45b8a5e7e69ab5c1acc7
kirishanthan <kiri.vara@outlook.com>
2016-05-13 17:22:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+var userChoice = "";
+$(document).ready(function(){
+    $(".rock").click(function(){
+      $(".paper").slideUp();
+      $(".scissors").slideUp();
+      userChoice = "Rock";
+      game();
+
+    });
+    $(".paper").click(function(){
+      $(".rock").slideUp();
+      $(".scissors").slideUp();
+      userChoice = "Paper";
+
+    });
+     $(".scissors").click(function(){
+      $(".rock").slideUp();
+      $(".paper").slideUp();
+      userChoice = "Scissors";
+
+    });
+});
+
+var game = function(){
+var cchoice = Math.random();
+var uchoice = userChoice;
+if (cchoice <= 0.33){
+  cchoice = "Rock";
+}
+else if(cchoice <= 0.66){
+  cchoice = "Paper";
+}
+else{
+  cchoice = "Scissors";
+}
+var results;
+
+if(uchoice === cchoice){
+    results = "Its a tie!";
+}
+else if(uchoice === "Rock"){
+  if(cchoice === "Paper"){
+    results = "Paper Wins";
+  }
+  else if(cchoice ==="Scissors"){
+    results = "Rock Wins";
+  }
+}
+else if(uchoice === "Paper"){
+  if(cchoice === "Scissors"){
+    results = "Scissors Wins";
+  }
+  else{
+    results = "Paper Wins";
+  }
+}
+
+else if(uchoice === "Scissors"){
+  if(cchoice === "Rock"){
+    results = "Rock Wins";
+  }
+  else{
+    results = "Scissors Wins";
+  }
+}
+document.write("<font size='24'><b>" + results + "</font></b><br>");
+document.write("<font size='14'>Your choice is " + uchoice + "</font><br>");
+document.write("<font size='14'>Computer's Choice is " + cchoice + "</font></b>");
+};
